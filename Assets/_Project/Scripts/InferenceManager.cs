a22029c8fd06a9fd6c366f58ce63e6763f42629b
yousan <ka1357amnbpdr@gmail.com>
2023-01-14 02:02:00 +0900

    feat: 数字のリアルタイム推論機能

---
+++ Assets/_Project/Scripts/InferenceManager.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UniRx;
+using Unity.Barracuda;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project
+{
+    public class InferenceManager : MonoBehaviour
+    {
+        private const int TexturePixelSize = 28;
+        [SerializeField] private NNModel model;
+        [SerializeField] private TextMeshProUGUI number;
+        [SerializeField] private RawImage image;
+        [SerializeField] private Button resetButton;
+        [SerializeField] private Paint paint;
+
+        private MnistModel _mnistModel;
+
+        private void Start()
+        {
+            _mnistModel = new MnistModel(model);
+            resetButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                number.text = string.Empty;
+                paint.ResetColors();
+            }).AddTo(this);
+        }
+
+        private void OnDestroy()
+        {
+            _mnistModel.Dispose();
+        }
+
+        private void Update()
+        {
+            var colors = (image.texture as Texture2D)?.GetPixels();
+            var pixels = new float[TexturePixelSize, TexturePixelSize];
+
+            for (var i = 0; i < TexturePixelSize * TexturePixelSize; i++)
+            {
+                if (colors != null) pixels[i % TexturePixelSize, i / TexturePixelSize] = colors[i].grayscale;
+            }
+
+            var result = _mnistModel.Execute(pixels);
+            number.text = result.ToString();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/LinqExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _Project
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<float> SoftMax(this IEnumerable<float> source)
+        {
+            var exp = source.Select(Mathf.Exp).ToArray();
+            var sum = exp.Sum();
+            return exp.Select(x => x / sum);
+        }
+    }
+}
